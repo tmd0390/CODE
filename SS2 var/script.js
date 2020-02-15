17be3af6df5afee8d6915a88e88a71da1cd66415
tmd <tmdaodieu@gmail.com>
2020-02-15 21:46:10 +0700

    buoidau

---
+++ SS2 var/script.js
@@ -0,0 +1,28 @@
+//alert("xin chao");// thong bao
+//prompt("ten ban la gi");// nhap thong tin nguoi dung
+
+let age; // khai bao biến
+age = 25; // gan gia tri
+let agel = 25
+//alert(age);
+//alert(agel);
+console.log(age);
+console.log(agel);
+
+
+var name;
+const PI = 3.14;
+
+let a = 5;
+let b = 10;
+let c = a + b;
+console.log(c);
+let d = a - b;
+let e = a * b;
+let f = a / b;
+console.log(d);
+console.log(e);
+console.log(f);
+
+
+
