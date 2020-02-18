09b29b4f1cc63bd0c79eab0ecb6371f1da055ca6
tmd <tmdaodieu@gmail.com>
2020-02-18 20:58:38 +0700

    ss3

--- SS2 var/script.js
+++ SS2 var/script.js
@@ -1,28 +1,39 @@
 //alert("xin chao");// thong bao
 //prompt("ten ban la gi");// nhap thong tin nguoi dung
 
-let age; // khai bao biến
-age = 25; // gan gia tri
-let agel = 25
-//alert(age);
-//alert(agel);
-console.log(age);
-console.log(agel);
+ let age; // khai bao biến
+// age = 25; // gan gia tri
+// let agel = 25
+// //alert(age);
+// //alert(agel);
+// console.log(age);
+// console.log(agel);
 
 
-var name;
-const PI = 3.14;
-
-let a = 5;
-let b = 10;
-let c = a + b;
-console.log(c);
-let d = a - b;
-let e = a * b;
-let f = a / b;
-console.log(d);
-console.log(e);
-console.log(f);
-
+// var name;
+// const PI = 3.14;
+// let a = 5;
+// let b = 10;
+// let c = a + b;
+// console.log(c);
+// let d = a - b;
+// let e = a * b;
+// let f = a / b;
+// console.log(d);
+// console.log(e);
+// console.log(f);
 
 
+// let yourName = prompt("Nhập tên");
+
+// let age = prompt("Nhập tuổi");
+// alert(yourName);
+// alert(age);
+// let yourAge = prompt("Nhập năm sinh");
+// alert(2020- yourAge );
+let a1 = prompt("Nhập số thứ nhất");
+let a2 = prompt("Nhập số thứ hai");
+alert(a1 + a2);
+alert(a1 - a2);
+alert(a1 * a2);
+alert(a1 / a2);
---
+++ SS3/script.js
@@ -0,0 +1,42 @@
+// let name = " Dương";
+// let name1 = 'Dương';
+// let nam2 = `Duong`;
+// let welcom = "xin chào";
+//let welcom1 = `${welcom} "c4ejs 71"`;
+//console.log(welcom1);
+
+// let x = 10;
+// let y;
+// y = 4 * (Math.pow(X,2) + 10 * x * Math.sqrt(Math.pow(X,2) + 3 * X + 1));
+// console.log(y);
+
+// let yourName = "Duong"
+// alert("xin chao" + yourName)
+// // chuoi ve so
+// let x = Number("5");
+// let y = Number("6");
+// console.log(x + y);
+// // chuoi ve so nguyen
+// x = parseInt("5.6");
+// console.log(x);
+// // chuoi ve so thuc
+// y = parseFloat("6.51111")
+// // so ve chuoi
+// // so + ''
+// let s = 10 +''; // s = "10";
+// let x =parseFloat( prompt("Mời nhập bán kính"));
+// let P = 2 * x * Math.PI
+// let S = x * x * Math.PI
+// console.log(`chu vi hinh tron ban kinh ${x} la:${P}`);
+// console.log(`die tich hinh tron ban kinh ${x} la: ${S}`);
+
+// alert(`chu vi hinh tron ban kinh ${x} la:${P}`)
+// let a = parseFloat(prompt("Mời nhập chiều dài"));
+// let b = parseFloat(prompt( "Mời nhập chiều rộng"));
+// let p = 2 * ( a + b);
+// let s = a * b
+// alert(`Chu vi hình chữ nhật chiều dài ${a} chiều rộng${b} là: ${p}`);
+// alert(`Diện tích hình chữ nhật chiều dài ${a} chiều rộng${b} là: ${s}`);
+
+
+
