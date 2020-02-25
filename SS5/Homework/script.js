391b81cb8c7e4b6101cd3a243299be9255c0adb9
tmd0390 <tmdaodieu@gmail.com>
2020-02-25 14:59:26 +0700

    ss5

---
+++ SS5/Homework/script.js
@@ -0,0 +1,52 @@
+//Bài1
+// let i = 0;
+// for (let i = 0; i <=100;i++){
+//    document.write(i+'-');
+//     console.log(i);
+    
+// }
+//Bài 2
+// let a = 100;
+// for(a=100;a>=0;a--){
+//     document.write(a+'-');
+//     console.log(a);
+    
+// }
+//Bài 3
+//số chẵn
+// let x = 0;
+// for(x=0;x<=100;x+=2){
+//     document.write(x+' ');
+//     console.log(x);
+    
+// }
+// số lẻ
+// let y = 1;
+// for(y=1;y<=100;y+=2){
+//     document.write(y+' ');
+//     console.log(y);
+    
+// }
+//Bài4
+// let x = 100;
+// for(x=100;x>=0;x-=2){
+//     document.write(x+' ');
+//     console.log(x);
+    
+// }
+// let y = 99;
+// for(y=99;y>=1;y-=2){
+//     document.write(y+' ');
+//     console.log(y);
+    
+// }
+//Bài 5
+let n = parseInt(prompt("Nhập số"));
+let s = 0;
+let i = 0;
+for(i=0;i<=n;i++){
+    s=s+i
+    
+}
+console.log(`Tổng của ${n}=${s}`);
+
